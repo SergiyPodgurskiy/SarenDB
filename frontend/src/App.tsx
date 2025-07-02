7f322c577d8d6283a344dfdda012bb189858ed35
SergiyPodgurskiy <Pshedgehog@gmail.com>
2025-07-02 10:28:05 +0300

    style: format code with Prettier

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,9 @@
 function App() {
-    return (
-        <div>
-            <h1 className="text-2xl">SarenDB Frontend</h1>
-        </div>
-    );
+  return (
+    <div>
+      <h1 className="text-2xl">SarenDB Frontend</h1>
+    </div>
+  );
 }
 
 export default App;
